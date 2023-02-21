0344e5a0679c65ff1f5c2001b84c5c938cbf8b89
govindkrishna03 <govindkrishna28@gmail.com>
2023-02-21 22:19:35 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,53 +1,40 @@
-import requests
-import os
-from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget, QPushButton, QListWidget, QListWidgetItem, QFrame, QFileDialog
-from PyQt5.QtGui import QPixmap, QIcon, QPainter, QBrush, QPalette
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QApplication, QMainWindow ,QLineEdit,QMessageBox
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QListWidget, QListWidgetItem, QFileDialog
+import sys,requests
+from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtCore import QBuffer, QByteArray, QIODevice
+
 from PyQt5.QtCore import Qt, QSize
-import sys
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 
-class MarsRoverApp(QWidget):
+
+class MarsRover(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super(MarsRover,self).__init__()
+        loadUi('main.ui',self)
 
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle('Martian Chronicles')
-        self.setGeometry(100, 100, 800, 600)
-
-    
-        self.setAutoFillBackground(True)
-        palette = self.palette()
-        palette.setBrush(QPalette.Window, QBrush(QPixmap("background.jpeg")))
-        self.setPalette(palette)
-
-        self.container = QFrame(self)
-        self.container.setFrameShape(QFrame.StyledPanel)
-        self.container.setFrameShadow(QFrame.Raised)
-
-        self.image_label = QLabel(self)
+        self.image_label=self.findChild(QLabel,"label_4")
         self.image_label.setAlignment(Qt.AlignCenter)
-
-        self.photo_list = QListWidget(self.container)
-        self.photo_list.itemClicked.connect(self.display_photo)
-
-        self.load_button = QPushButton('Load Photos', self.container)
-        self.load_button.clicked.connect(self.load_photos)
+        self.photo_list =self.findChild(QListWidget,"listWidget")
+        self.loadbutton=self.findChild(QPushButton,"pushButton")
+        self.downloadbutton=self.findChild(QPushButton,"pushButton_2")
+        self.sendbutton=self.findChild(QPushButton,"pushButton_3")
+        self.subject=self.findChild(QLineEdit,"lineEdit_2")
+        self.attachbutton=self.findChild(QPushButton,"pushButton_4")
+        self.recipients = self.findChild(QLineEdit,"lineEdit")
         
 
-        self.download_button = QPushButton('Download Image', self.container)
-        self.download_button.clicked.connect(self.download_image)
-        self.download_button.setEnabled(False)
+        self.photo_list.itemClicked.connect(self.display_photo)
+        self.loadbutton.clicked.connect(self.load_photos)
+        self.downloadbutton.clicked.connect(self.download_image)
+        self.attachbutton.clicked.connect(self.attach_image)
+        self.sendbutton.clicked.connect(self.send_email)
+        self.downloadbutton.setEnabled(False)
 
-        layout = QVBoxLayout(self.container)
-        layout.addWidget(self.photo_list)
-        layout.addWidget(self.load_button)
-        layout.addWidget(self.download_button)
-
-
-        main_layout = QVBoxLayout(self)
-        main_layout.addWidget(self.image_label)
-        main_layout.addWidget(self.container)
 
     def load_photos(self):
     
@@ -68,8 +55,9 @@ class MarsRoverApp(QWidget):
             item.setText(f"Caption: {photo['camera']['full_name']}\nCamera Type: {photo['camera']['name']}\nLocation: {photo['rover']['name']}")
             self.photo_list.addItem(item)
 
-        self.load_button.hide()
-        self.download_button.setEnabled(True)
+        self.loadbutton.hide()
+        self.downloadbutton.setEnabled(True)
+
 
     def display_photo(self, item):
         pixmap = item.icon().pixmap(QSize(640, 480))
@@ -79,20 +67,84 @@ class MarsRoverApp(QWidget):
     def download_image(self):
         if not hasattr(self, 'selected_photo_url'):
             return
-        
-        filename, _ = QFileDialog.getSaveFileName(self, 'Save Image', '', 'JPEG files (*.jpg);;All files (*.*)')
+
+        response = requests.get(self.selected_photo_url)
+        self.selected_photo_data = response.content
+
+        filename, _ = QFileDialog.getSaveFileName(self, 'Save Image', '', '')
         if not filename:
             return
 
-        response = requests.get(self.selected_photo_url)
         with open(filename, 'wb') as f:
-            f.write(response.content)
-            f.close()
+            f.write(self.selected_photo_data)
 
         print(f'Successfully downloaded image to {filename}')
 
+    def attach_image(self):
+        filename, _ = QFileDialog.getOpenFileName(self, 'Open File', '', '')
+
+        if filename:
+            pixmap = QPixmap(filename)
+            self.selected_image = pixmap.toImage()
+            self.attachbutton.setText('Attached')
+
+    def send_email(self):
+        recipients = self.recipients.text().split(',')
+        subject = 'MarsRover'
+        
+
+        if not recipients:
+            QMessageBox.warning(self, 'Error', 'Please enter at least one recipient.')
+            return
+
+        if not subject:
+            QMessageBox.warning(self, 'Error', 'Please enter a subject.')
+            return
+
+        if not self.selected_image:
+            QMessageBox.warning(self, 'Error', 'Please attach an image.')
+            return
+
+        try:
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login('gk5974518@gmail.com', 'zfzjrlzmxkerqsvs')
+
+            msg = MIMEMultipart()
+            msg['From'] = 'gk5974518@gmail.com'
+            msg['To'] = ', '.join(recipients)
+            msg['Subject'] = subject
+
+
+            image_bytes = QByteArray()
+            buffer = QBuffer(image_bytes)
+            buffer.open(QIODevice.WriteOnly)
+            self.selected_image.save(buffer, "JPEG")
+            img = MIMEImage(image_bytes)
+            img.add_header('Content-Disposition', 'attachment', filename='')
+            msg.attach(img)
+
+            server.sendmail('gk5974518@gmail.com', recipients, msg.as_string())
+            server.quit()
+
+            QMessageBox.information(self, 'Success', 'Email sent successfully.')
+            self.close()
+
+        except Exception as e:
+            QMessageBox.warning(self, 'Error', f'Error sending email: {e}')
+
+            server.quit()
+
+            self.close()
+    
+    
+
+    
+
+
+
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MarsRoverApp()
+    app =QApplication(sys.argv)
+    window = MarsRover()
     window.show()
-    sys.exit(app.exec_())
+    app.exec_()
---
+++ readme.md
@@ -0,0 +1,10 @@
+This is my first ever GUI APP using Python language.
+ 
+
+
+It was really nice making this app and i felt so happy after completing this.
+It took me almost 1week + finish off the task..
+
+The previous project AutomatetheBoringStuff was really helpfull in doing this task.
+Researched a lot.
+Couldnt make the GUI interface as i had lot of class works.....
